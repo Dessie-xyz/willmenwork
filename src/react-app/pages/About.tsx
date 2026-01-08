be279c74d2046b72c2cff6ed03b6eea63451bf08
Harper <154289487+O5Desmos@users.noreply.github.com>
2026-01-07 23:33:43 -0500

    BRANDING

--- src/react-app/pages/About.tsx
+++ src/react-app/pages/About.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <h1>About</h1>
         <p style={{textAlign:"left", paddingLeft:"5%", paddingRight:"5%"}}>Hi,<br/><br/>
         I’m [REDACTED], but you can call me Red, the founder and curator of Weak Submissive Men. As a rather financially irresponsible lesbian, my initial acquisition of this website was an impulsive decision while drinking at my Cloudflare dashboard. Since then, I have sobered up and have committed myself to using my formidable SEO skills to better the weak-submissive-man-appreciating community. This website is dedicated to providing a central hub for any media surrounding weak submissive men.<br/><br/>
-        <b>However, I need your help.</b> Despite my unchallenged intellect and impeccable branding skills, I’m just one idiot and can’t find all of the highest quality content by myself. That’s why community engagement is a core value of weaksubmissive.men. We encourage anyone who has any contributions or suggestions reach out using the means listed on our <Link to="/submit">submissions page</Link>. Though this website’s creation may have been unplanned, I promise mommy still loves you very much. 
+        <b>However, I need your help.</b> Despite my unchallenged intellect and impeccable branding skills, I’m just one idiot and can’t find all of the highest quality content by myself. That’s why community engagement is a core value of Weak Submissive Men. We encourage anyone who has any contributions or suggestions reach out using the means listed on our <Link to="/submit">submissions page</Link>. Though this website’s creation may have been unplanned, I promise mommy still loves you very much. 
         <br/><br/>
         With much love,
         <br />Red</p><br/>
